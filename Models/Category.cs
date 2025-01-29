69b8f860b1abb390d7590fda0115c2e813b132f1
nurnoby admin <nurnoby323@gmail.com>
2025-01-29 11:02:07 +0600

    category model created

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_CURD_core.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        public double Price { get; set; }
+    }
+}
