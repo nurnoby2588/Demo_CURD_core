d0822e75e76115358b07a22a01451192730b41fa
nurnoby admin <nurnoby323@gmail.com>
2025-02-07 12:11:34 +0600

    edit?update category done

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -22,11 +22,37 @@ namespace Demo_CURD_core.Controllers
         }
         [HttpPost]
         public IActionResult CreateCategory(Category category)
-        { 
-            
-            _DbContext.Categories.Add(category);
-            _DbContext.SaveChanges();
-            return RedirectToAction("Index","Category"); 
+        {
+
+            if (ModelState.IsValid) {
+                _DbContext.Categories.Add(category);
+                _DbContext.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        // edit/updat
+        public IActionResult EditCategory(int? CategoryID)
+        {
+            if (CategoryID == 0 || CategoryID == null) { return NotFound(); }
+            // CatergoryContoller all value in _DBContext
+            Category? category = _DbContext.Categories.Find(CategoryID);
+            // another way find .collum ar jei value r first a match korbe oi value niye asbe.
+            Category? category1 = _DbContext.Categories.FirstOrDefault(c=>c.CategoryID == CategoryID);
+            if (category1 == null) { return NotFound(); }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult EditCategory(Category category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _DbContext.Categories.Update(category);
+                _DbContext.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
         }
     }
 }
