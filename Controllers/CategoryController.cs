df736a56987cdc5e4d0644ddcc02e87256a32ec5
nurnoby admin <nurnoby323@gmail.com>
2025-02-07 09:26:23 +0600

    get data from database

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo_CURD_core.Data;
+using Demo_CURD_core.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_CURD_core.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _DbContext;
+        public CategoryController(ApplicationDBContext DBContext)
+        {
+                 _DbContext = DBContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _DbContext.Categories.ToList();
+            return View(categories);
         }
     }
 }
